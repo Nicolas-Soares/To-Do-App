b8a0c6a2dde9d2f4c4b132d03760d7e2f2f007d6
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-06-11 13:57:50 -0300

    Project initialization
    Added base files;

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
+    username: 'crwdttykteusle',
+    password: '5d445c5a7954f35c58f73c1f5475b776ff4f4022f4ec876e56aca3896b452aa8',
+    database: 'ddl0nnorqbf7hg',
+    define: {
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+    },
+}
