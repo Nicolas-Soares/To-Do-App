4fe4c68538bcf914f5a8055a05c8ad142d7073f9
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-06-11 14:33:48 -0300

    Project update

--- src/app.js
+++ src/app.js
@@ -8,6 +8,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
-    console.log('Server running on port 3333')
-})
+app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'))
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
+routes.get('/', function (req, res) {
+    res.send('Hello')
+})
+
 module.exports = routes
