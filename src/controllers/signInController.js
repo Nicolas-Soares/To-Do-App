da1e3338b586989675a8b8a881d419c695199250
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-06-17 03:02:13 -0300

    Email verification added to signInController / Added email-existence to package.json / Added a homepage button

--- src/controllers/signInController.js
+++ src/controllers/signInController.js
@@ -1,16 +1,22 @@
-const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const emailExistence = require('email-existence')
+const User = require('../models/User')
 
 module.exports = {
     async signIn(req, res) {
         const { username, email, password } = req.body
-        
-        let encryptedPassword = await bcrypt.hash(password, 5)
 
-        let formatedUsername = username.replace(/\s+/g, ' ')
-        
-        await User.create({ username: formatedUsername , email, password: encryptedPassword })
+        await emailExistence.check(email, async function (error, response) {
+            if (response === false) {
+                return res.send('Inexistent or wrong email')
+            }
 
-        return res.redirect('/')
+            let encryptedPassword = await bcrypt.hash(password, 5)
+            let formatedUsername = username.replace(/\s+/g, ' ')
+
+            await User.create({ username: formatedUsername, email, password: encryptedPassword })
+
+            return res.redirect('/')
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,22 @@ const loginController = require('./controllers/loginController')
 
 const routes = express.Router()
 
+//ROOT -----
 routes.get('/', function(req, res) {
     res.render('index', {
         title: 'Login',
-        css: ['indexStyle.css']
+        css: ['indexStyle.css', 'defaultStyle.css']
     })
 })
 routes.post('/', loginController.login)
 
+//CREATE ACCOUNT -----
 routes.get('/create-account', function(req, res) {
     res.render('signForm', { title: 'Sign In', css: ['defaultStyle.css', 'signFormStyle.css'] })
 })
 routes.post('/create-account',  signInController.signIn)
 
+//TASKS -----
 routes.get('/tasks', function(req, res) {
     res.render('tasks', {
         content: 'Hello',
